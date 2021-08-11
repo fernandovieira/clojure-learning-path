a7739a6dea65b036accf87eb7334839efc645519
fernandovieira <fernandonascvieira@gmail.com>
2021-08-10 23:33:19 -0300

    Part 1 solved

--- src/adventofcode/firstday.clj
+++ src/adventofcode/firstday.clj
@@ -1,20 +1,27 @@
-(ns adventofcode.firstday)
-
+(ns adventofcode.firstday
+  (:require [clojure.string :as str])
+  (:require [clojure.java.io :as input-out]))
 
 (defn findMyStar
   "Find all my fifty star"
   [expenses]
   (loop [remaning-expenses expenses]
-       (let [[first_elem & remainder] remaning-expenses]
-         (if (empty? remaning-expenses) 0
-         (let [result (reduce (fn[x y]
-                                (if (= (+ x y) 2020) (reduced  (* x y))  first_elem)) first_elem remainder)]
-          (if (= result first_elem) (recur remainder) result)))))
+    (let [[first_elem & remainder] remaning-expenses]
+      (if (empty? remaning-expenses) 0
+          (let [result (reduce (fn[x y]
+                                 (if (= (+ x y) 2020) (reduced (* x y))  first_elem)) first_elem remainder)]
+            (if (= result first_elem) (recur remainder) result)))))
+            ;;#dbg ^{:break/when (=  result first_elem)}
 )
-
 
 (defn readFileToList
   "Read a file with entries and return a list"
   []
-  (def entries (list 1 2 3 ))
-  entries)
+  (def entries (str/split (slurp (input-out/resource "entries.txt")) #" "))
+  (apply list (map #(Integer/parseInt (str/trim %)) entries)))
+
+(defn resolveProblem
+  "Now resolving the problem"
+  []
+  (def entries (readFileToList))
+  (let [result (findMyStar entries)]))
--- test/adventofcode/firstday_test.clj
+++ test/adventofcode/firstday_test.clj
@@ -3,7 +3,7 @@
             [adventofcode.firstday :refer :all]))
 
 
-(def advocateExample (list 1721 979 366 299 675 1456))
+(def advocateExample (list 979 1721 366 299 675 1456))
 
 (def advocateExampleFalse (list 1721 979 366 300 675 1456))
 
@@ -15,9 +15,16 @@
 (deftest mustReturnZero
   (testing "Test when there isn't any star"
     (let [result (findMyStar advocateExampleFalse)]
-      (is ( = result 0)))))
+       (is ( = result 0)))))
 
 (deftest mustReturnaList
   (testing "Read a file and must return a list"
-    (let [mylist (readFile)]
+    (let [mylist (readFileToList)]
+      (let [[first & rem] mylist])
       (is (list? mylist)))))
+
+(deftest mustReturn1006176
+  (testing "This is solve the advent of code day 1"
+   (let [result (resolveProblem)]
+     (is (= 1006176 result))))
+)
