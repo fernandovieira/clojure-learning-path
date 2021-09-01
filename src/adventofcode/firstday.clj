04d89fd9f8c22fa3adf8e8525330a4da2b3664de
fernandovieira <fernandonascvieira@gmail.com>
2021-08-31 21:37:29 -0300

    Start to solve the second part of the problem

--- src/adventofcode/firstday.clj
+++ src/adventofcode/firstday.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as str])
   (:require [clojure.java.io :as input-out]))
 
-(defn findMyStar
+(defn solvePartI
   "Find all my fifty star"
   [expenses]
   (loop [remaning-expenses expenses]
@@ -10,9 +10,7 @@
       (if (empty? remaning-expenses) 0
           (let [result (reduce (fn[x y]
                                  (if (= (+ x y) 2020) (reduced (* x y))  first_elem)) first_elem remainder)]
-            (if (= result first_elem) (recur remainder) result)))))
-            ;;#dbg ^{:break/when (=  result first_elem)}
-)
+            (if (= result first_elem) (recur remainder) result))))))
 
 (defn readFileToList
   "Read a file with entries and return a list"
@@ -24,5 +22,26 @@
   "Now resolving the problem"
   []
   (def entries (readFileToList))
-  (let [result (findMyStar entries)]
-    result))
+  (let [result (solvePartI entries)] result))
+
+(defn produceTheAnswer
+  "Multiplying them together must produce the answer"
+  [myList]
+  (def answer (reduce + myList))
+  (if (= answer 2020) (reduce * myList) nil))
+
+(def runThroughList
+  "Run through a list sum"
+  [firstElement expense]
+
+)
+
+(defn solvePartII
+  "Now three elements that sum it is equal 2020"
+  []
+  (def my_test '(1 3 4 2001 18 234 2))
+  (loop [my_list my_test]
+    (if (not= nil my_list)
+      (let [[firstItem & reminder] my_list]
+        (print firstItem)
+        (recur reminder)))))
